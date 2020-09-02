611b132ac180b8a9c2352c4d84c059693dc2ee6f
aadilraza339 <aadilraza339@gmail.com>
2020-09-02 15:40:08 +0530

    add some features

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/codechef.jpg') {
+      myImage.setAttribute('src','images/download.jpeg');
+    } else {
+      myImage.setAttribute('src','images/codechef.jpg');
+    }
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'codechef is cool, ' + myName;
+  }
+ 
+
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'codechef is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+ 
+  function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
